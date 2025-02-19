64a37e2f6b8f9b4dbb85eee0d7ab33fc588a5f7d
vrunoa <vruno.alassia@gmail.com>
2025-02-18 21:19:20 -0800

    split

---
+++ go/lint.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+
+	"dagger/go/internal/dagger"
+)
+
+const (
+	golangciImage = "golangci/golangci-lint:v1.61-alpine"
+)
+
+// Lint - Lint the Go source code with golangci-lint
+func (m *Go) Lint(ctx context.Context, src *dagger.Directory) (string, error) {
+	return dag.Container().
+		From(golangciImage).
+		WithWorkdir("/src").
+		WithMountedDirectory("/src", src).
+		WithEnvVariable("GOWORK", "off").
+		WithExec([]string{"golangci-lint", "run", "/src/...", "-v", "--timeout=10m"}).
+		Stdout(ctx)
+}
