22f3cfab3edf4c876eb2a3897b767e9057a0435b
vrunoa <vruno.alassia@gmail.com>
2025-03-15 22:17:34 -0700

    add godot dag

---
+++ godot/gut.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+
+	"github.com/vrunoa/daggerverse/godot/internal/dagger"
+)
+
+const (
+	godotImage = "barichello/godot-ci:4.4"
+)
+
+// Test running go test
+func (m *Godot) Gut(ctx context.Context, src *dagger.Directory, testPath string) (*dagger.File, error) {
+	cont, err := dag.Container().
+		From(godotImage).
+		WithWorkdir("/src").
+		WithMountedDirectory("/src", src).
+		WithEnvVariable("GOWORK", "off").
+		WithExec([]string{
+			"godot",
+			"-d",
+			"-s",
+			"--headless",
+			"--path",
+			"/src",
+			"./addons/gut/gut_cmdln.gd",
+			"-gdir",
+			testPath,
+			"-gexit",
+			"-gjunit_xml_file",
+			"coverage.xml",
+		}).
+		Sync(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return cont.File("coverage.xml"), nil
+}
---
+++ godot/main.go
@@ -0,0 +1,3 @@
+package main
+
+type Godot struct{}
