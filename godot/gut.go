7433247d6c8c64f70be56d2f632d4901a9ed101e
vrunoa <vruno.alassia@gmail.com>
2025-09-13 11:35:48 -0700

    remove visual

--- godot/gut.go
+++ godot/gut.go
@@ -43,37 +43,3 @@ func (m *Godot) Gut(ctx context.Context, src *dagger.Directory, testPath string)
 	return cont.File("coverage.xml"), nil
 }
 
-func (m *Godot) XbfvGut(ctx context.Context, src *dagger.Directory, testPath string) (*dagger.File, error) {
-	cont, err := dag.Container().
-		From(godotImage).
-		WithWorkdir("/src").
-		WithMountedDirectory("/src", src).
-		WithEnvVariable("GOWORK", "off").
-		WithExec([]string{
-			"godot",
-			"--headless",
-			"--import",
-		}).
-		WithExec([]string{
-			"xvfb-run",
-			"-a",
-			"godot",
-			"-d",
-			"-s",
-			"--headless",
-			"--path",
-			"/src",
-			"./addons/gut/gut_cmdln.gd",
-			"-gdir",
-			testPath,
-			"-gexit",
-			"-gjunit_xml_file",
-			"coverage.xml",
-		}).
-		Sync(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return cont.File("coverage.xml"), nil
-}
-
